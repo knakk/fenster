d997a449e8baf3fea104e12ff3903bf8bf3b881d
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-09-28 15:41:06 +0200

    dont use color in casperjs reports (cause jenkins cant show colors)

--- integration-tests.sh
+++ integration-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ./fenster & echo $! > fenster.pid
 sleep 1 # give the server time to start
-casperjs test browser_test.js
+casperjs test browser_test.js --no-colors
 kill $(cat fenster.pid)
 rm fenster.pid
