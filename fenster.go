4840254cb3ad0958bc759333a187a9f9faaffbfa
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-10-02 20:59:53 +0200

    refactor rdf/json/html handler

--- fenster.go
+++ fenster.go
@@ -34,43 +34,12 @@ var (
 
 type mainHandler struct{}
 
-// handler for displaying the RDF resource
-func (m mainHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		http.Redirect(w, r, conf.UI.RootRedirectTo, http.StatusFound)
-		return
-	}
-
-	var uri, q string
-	resolved := false
-	format := "json"
-	suffix := regexp.MustCompile(`\.[a-z1-9]+$`).FindString(r.URL.Path)
-	switch suffix {
-	case "":
-		break
-	case ".html":
-		uri = conf.BaseURI + strings.TrimSuffix(r.URL.Path, ".html")
-		q = fmt.Sprintf(query, uri, uri, conf.QuadStore.ResultsLimit)
-		format = "html"
-		resolved = true
-	case ".json":
-		uri = conf.BaseURI + strings.TrimSuffix(r.URL.Path, ".json")
-		q = fmt.Sprintf(query, uri, uri, conf.QuadStore.ResultsLimit)
-		resolved = true
-	case ".rdf":
-		uri = conf.BaseURI + strings.TrimSuffix(r.URL.Path, ".rdf")
-		q = fmt.Sprintf(query2, uri, uri, uri, uri)
-		format = "rdf"
-		resolved = true
-	default:
-		errorHandler(w, r, fmt.Sprintf("Unsupported output format: %s.\n\nValid formats are: html, json, rdf", suffix[1:]), http.StatusBadRequest)
-		return
-	}
-
-	if !resolved {
-		http.Redirect(w, r, r.URL.Path+".html", http.StatusFound)
-		return
-	}
+// rdfHandler serves the quads in TriG syntax
+// http://wifo5-03.informatik.uni-mannheim.de/bizer/trig/
+func rdfHandler(w http.ResponseWriter, r *http.Request) {
+	uri := conf.BaseURI + strings.TrimSuffix(r.URL.Path, ".rdf")
+	format := "rdf"
+	q := fmt.Sprintf(query2, uri, uri, uri, uri)
 
 	resp, err := sparql.Query(conf.QuadStore.Endpoint, q, format,
 		time.Duration(conf.QuadStore.OpenTimeout)*time.Millisecond, time.Duration(conf.QuadStore.ReadTimeout)*time.Millisecond)
@@ -79,20 +48,86 @@ func (m mainHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if format != "html" {
-		switch format {
-		case "json":
-			w.Header().Set("Content-Type", "application/json")
-		case "rdf":
-			w.Header().Set("Content-Type", "application/x-trig")
-		}
-		io.WriteString(w, string(resp))
+	w.Header().Set("Content-Type", "application/x-trig")
+	io.WriteString(w, string(resp))
+}
+
+// jsonHandler serves the raw "application/sparql-results+json" results from
+// the SPARQL endpoint
+func jsonHandler(w http.ResponseWriter, r *http.Request) {
+	uri := conf.BaseURI + strings.TrimSuffix(r.URL.Path, ".json")
+	q := fmt.Sprintf(query, uri, uri, conf.QuadStore.ResultsLimit)
+	format := "json"
+
+	resp, err := sparql.Query(conf.QuadStore.Endpoint, q, format,
+		time.Duration(conf.QuadStore.OpenTimeout)*time.Millisecond, time.Duration(conf.QuadStore.ReadTimeout)*time.Millisecond)
+	if err != nil {
+		errorHandler(w, r,
+			err.Error()+`. Refresh to try again.\n\n
+			You can increase the timeout values in Fensters configuration file.`,
+			http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w, string(resp))
+}
+
+// mainHandler serves the resource HTML presentation, or dispatches to the
+// rdfHandler or jsonHandler
+func (m mainHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		// redirect from root to info page
+		http.Redirect(w, r, conf.UI.RootRedirectTo, http.StatusFound)
+		return
+	}
+
+	var uri string
+	resolved := false
+	suffix := regexp.MustCompile(`\.[a-z1-9]+$`).FindString(r.URL.Path)
+
+	switch suffix {
+	case "":
+		break
+	case ".html":
+		uri = conf.BaseURI + strings.TrimSuffix(r.URL.Path, ".html")
+		resolved = true
+	case ".json":
+		jsonHandler(w, r)
+		return
+	case ".rdf":
+		rdfHandler(w, r)
+		return
+	default:
+		errorHandler(w, r,
+			fmt.Sprintf(`Unsupported output format: %s.\n\n
+				Valid formats are: html, json, rdf`, suffix[1:]), http.StatusBadRequest)
+		return
+	}
+
+	// The URI should be an exlusive identifier of the resource; so we redirect
+	// to URI+.html
+	if !resolved {
+		http.Redirect(w, r, r.URL.Path+".html", http.StatusFound)
+		return
+	}
+
+	q := fmt.Sprintf(query, uri, uri, conf.QuadStore.ResultsLimit)
+	resp, err := sparql.Query(conf.QuadStore.Endpoint, q, "json",
+		time.Duration(conf.QuadStore.OpenTimeout)*time.Millisecond,
+		time.Duration(conf.QuadStore.ReadTimeout)*time.Millisecond)
+	if err != nil {
+		errorHandler(w, r,
+			err.Error()+`. Refresh to try again.\n\nYou can increase the timeout
+			values in Fensters configuration file.`, http.StatusInternalServerError)
 		return
 	}
 
 	res, err := sparql.ParseJSON(resp)
 	if err != nil {
-		errorHandler(w, r, "Failed to parse JSON response from remote SPARQL endpoint.", http.StatusInternalServerError)
+		errorHandler(w, r,
+			"Failed to parse JSON response from remote SPARQL endpoint.",
+			http.StatusInternalServerError)
 		return
 	}
 
